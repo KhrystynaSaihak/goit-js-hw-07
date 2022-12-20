056e5df8a336f41293d53c91d82f8c2a722ec1c1
Khrystyna Saihak <anastafisher@gmail.com>
2022-12-20 20:10:19 +0200

    task1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,61 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+    galleryContainer: document.querySelector('.gallery'),
+}
+
+const createGalleryMarkup = (galleryItems) => {
+    return galleryItems.map(item => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+      target="_blank"
+      rel="noreferrer noopener"
+    />
+  </a>
+</div>`
+    }).join('');
+};
+
+const onEscKeyPress = (e, modal) => {
+    const ESC_KEY_CODE = 'Escape';
+    if (e.code === ESC_KEY_CODE) {
+        modal.close(); 
+    }
+}
+
+const getPreviewImgUrl = el => {
+    return el.dataset.source;
+};
+
+const showBasicLightbox = previewImgUrl => {
+    const modal = basicLightbox.create(`
+		<img width="1400" height="900" src="${previewImgUrl}">`);
+    modal.show();
+
+    window.addEventListener('keydown', (e) => onEscKeyPress(e,modal), {once:true});
+};
+
+const onGalleryContainerClick = (e) => {
+    e.preventDefault();
+    const currentEl = e.target;
+    const isGalleryItem = currentEl.classList.contains('gallery__image');
+
+    if (!isGalleryItem) {
+        return
+    }
+
+    const previewImgUrl = getPreviewImgUrl(currentEl);
+    showBasicLightbox(previewImgUrl);
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+refs.galleryContainer.innerHTML = galleryMarkup;
+
+refs.galleryContainer.addEventListener('click', onGalleryContainerClick);
